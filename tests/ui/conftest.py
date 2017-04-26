5d7645ca8dda9628033b374887d10cbaabb1a190
David Baumgold <david@davidbaumgold.com>
2017-04-26 09:46:47 -0400

    another test

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -4,7 +4,8 @@ from ui.models import Video
 
 
 @pytest.fixture
-def user(django_db_setup):
+@pytest.mark.django_db
+def user():
     obj = User(username="example", email="example@mit.edu")
     obj.set_password("ex4mple")
     obj.save()
@@ -12,11 +13,13 @@ def user(django_db_setup):
 
 
 @pytest.fixture
-def bunny(user):
+def video(user):
     obj = Video(
         creator=user,
         s3_object_key="BigBuckBunny.m4v",
         source_url="https://dl.dropboxusercontent.com/1/view/abc123/BigBuckBunny.m4v",
+        title="Big Buck Bunny",
+        description="Open source video from Blender",
     )
     obj.save()
     return obj
--- tests/ui/test_models.py
+++ tests/ui/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from boto3.resources.base import ServiceResource
 from django.db import IntegrityError
 from django.contrib.auth.models import User
 from ui.models import Video
@@ -7,11 +8,25 @@ from ui.models import Video
 pytestmark = pytest.mark.django_db
 
 
-def test_s3_object_uniqueness(user, bunny):
-    video = Video(
+def test_s3_object_uniqueness(user, video):
+    video2 = Video(
         creator=user,
-        s3_object_key=bunny.s3_object_key,
+        s3_object_key=video.s3_object_key,
         source_url="https://example.com/context.mp4",
     )
     with pytest.raises(IntegrityError):
-        video.save()
+        video2.save()
+
+
+def test_video_aws_integration(video):
+    s3_obj = video.s3_object
+    assert isinstance(s3_obj, ServiceResource)
+    assert s3_obj.key == video.s3_object_key
+    s3_url = video.s3_url
+    assert isinstance(s3_url, str)
+    assert s3_url.startswith("https://s3.amazonaws.com/")
+    cf_url = video.cloudfront_url
+    assert isinstance(cf_url, str)
+    assert cf_url.startswith("https://video-cf.cloudfront.net/")
+    assert callable(video.cloudfront_signed_url)
+
