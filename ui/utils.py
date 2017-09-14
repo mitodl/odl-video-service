eb028971dc6e9c12f9fff74742bed09a3ca62c22
George Schneeloch <gschneel@mit.edu>
2017-09-14 13:11:43 -0400

    Lint

--- ui/utils.py
+++ ui/utils.py
@@ -8,10 +8,6 @@ import re
 import boto3
 from django.conf import settings
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
 from mit_moira import Moira
 
 log = logging.getLogger(__name__)
