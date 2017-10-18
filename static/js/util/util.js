29b7a1ec824f3ca7183284f69eea8dc827cb7812
George Schneeloch <gschneel@mit.edu>
2017-10-18 16:48:44 -0400

    Upgrade node.js and yarn

--- static/js/util/util.js
+++ static/js/util/util.js
@@ -3,3 +3,9 @@
 export function getDisplayName(WrappedComponent: ReactClass<*>) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
+
+/**
+ * Returns a promise which resolves after a number of milliseconds have elapsed
+ */
+export const wait = (millis: number): Promise<void> =>
+  new Promise(resolve => setTimeout(resolve, millis));
