d9ba49ce988ecf01de03acd02ff853a1961f2504
George Schneeloch <gschneel@mit.edu>
2018-08-29 16:34:30 -0400

    Remove IS_OSX now that Docker for Mac is used by everyone

--- webpack_dev_server.sh
+++ webpack_dev_server.sh
@@ -7,18 +7,8 @@ source ./scripts/envs.sh
 WEBPACK_HOST='0.0.0.0'
 WEBPACK_PORT='8082'
 
-# The webpack server should only be run in one of two cases:
-#    1) We are running Linux and inside the Docker container
-#    2) We're on an OSX host machine. Our current workflow for developing on a Docker container involves running
-#       the webpack server on the host machine rather than the container.
-# If neither of those are true, running this script is basically a no-op.
-
-if [[ "$IS_OSX" == "true" && "$INSIDE_CONTAINER" == "true" ]] ; then
-  echo -e "EXITING WEBPACK STARTUP SCRIPT\nOSX Users: The webpack dev server should be run on your host machine."
-else
-  if [[ "$1" == "--install" ]] ; then
+if [[ "$1" == "--install" ]] ; then
     yarn install --frozen-lockfile && echo "Finished yarn install"
-  fi
-  # Start the webpack dev server on the appropriate host and port
-  node ./hot-reload-dev-server.js --host "$WEBPACK_HOST" --port "$WEBPACK_PORT"
 fi
+# Start the webpack dev server on the appropriate host and port
+node ./hot-reload-dev-server.js --host "$WEBPACK_HOST" --port "$WEBPACK_PORT"
