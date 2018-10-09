e59686dfad343158c1927432fee9d9189be7ef99
Doof <mitx-devops@mit.edu>
2018-10-09 19:59:48 +0000

    Release 0.31.0

--- odl_video/settings.py
+++ odl_video/settings.py
@@ -20,7 +20,7 @@ from odl_video.envs import (
     parse_env
 )
 
-VERSION = "0.30.0"
+VERSION = "0.31.0"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 parse_env(f'{BASE_DIR}/.env')
