45de12d7d180934a30f32c7a94e9db80c964bd25
Matt Bertrand <mattbert@mit.edu>
2019-01-04 09:44:59 -0500

    Handle missing analytics times

--- static/js/components/analytics/AnalyticsChart.js
+++ static/js/components/analytics/AnalyticsChart.js
@@ -105,14 +105,15 @@ export class AnalyticsChart extends React.Component {
     const chartBodyClipPathId = `${this._namespace}-chart-body-clipPath`
     const chartBodyBounds = this._getRelativeChartBodyBounds()
     const yTicks = this._getYTicks({ analyticsData })
+    const xMax =
+      duration > 0
+        ? duration / 60
+        : analyticsData.times ? analyticsData.times.slice(-1)[0] : 0
     return (
       <VictoryChart
         {...dimensions}
         domain={{
-          x: [
-            0,
-            duration > 0 ? duration / 60 : analyticsData.times.slice(-1)[0]
-          ]
+          x: [0, xMax]
         }}
         domainPadding={{ x: [0, 0], y: [0, 0] }}
         padding={padding}
