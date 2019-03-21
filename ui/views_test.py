adb08065c360c814dc915c254ad2afe560cca0d2
tasawernawaz <tasawer.nawaz@arbisoft.com>
2019-03-21 17:22:28 +0500

    test updated

--- ui/views_test.py
+++ ui/views_test.py
@@ -1232,33 +1232,19 @@ def test_terms_of_service_for_logged_in_user(mock_user_moira_lists, owns_collect
     assert js_settings["editable"] == owns_collections or is_staff
 
 
-def test_moira_list_users_permission(logged_in_apiclient):
+@pytest.mark.parametrize(
+    'url',
+    [
+        reverse('member-lists', kwargs={'username_or_email': 'test_user'}),
+        reverse('list-members', kwargs={'list_name': 'test_name'}),
+    ]
+)
+def test_moira_list_views_permission(logged_in_apiclient, url):
     """
-    Tests that only authenticated users with admin permissions can call MoiraListsForUser
-    """
-    client, user = logged_in_apiclient
-    client.logout()
-    url = reverse('member-lists', kwargs={'username_or_email': 'test_user'})
-
-    # call with anonymous user
-    assert client.get(url).status_code == status.HTTP_403_FORBIDDEN
-    # call with another user not on admin list
-    client.force_login(user)
-    assert client.get(url).status_code == status.HTTP_403_FORBIDDEN
-    # call with user on admin list
-    user.is_staff = True
-    user.save()
-    client.force_login(user)
-    assert client.get(url).status_code == status.HTTP_200_OK
-
-
-def test_users_moira_lists_permission(logged_in_apiclient):
-    """
-    Tests that only authenticated users with admin permissions can call UsersForMoiraList
+    Tests that only authenticated users with admin permissions can call MoiraListsForUser and UsersForMoiraList
     """
     client, user = logged_in_apiclient
     client.logout()
-    url = reverse('list-members', kwargs={'list_name': 'test_name'})
 
     # call with anonymous user
     assert client.get(url).status_code == status.HTTP_403_FORBIDDEN
