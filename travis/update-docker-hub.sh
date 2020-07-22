0a43fbf7cb5ea58f68c9b416f1988dcec5d19940
Matt Bertrand <mattbert@mit.edu>
2020-07-22 14:47:55 -0400

    travis

--- travis/update-docker-hub.sh
+++ travis/update-docker-hub.sh
@@ -2,7 +2,5 @@
 set -eo pipefail
 
 docker build -t mitodl/odl_video_service_web_travis_next -f Dockerfile .
-docker build -t mitodl/odl_video_service_watch_travis -f travis/Dockerfile-travis-watch-build .
 
 docker push mitodl/odl_video_service_web_travis_next
-docker push mitodl/odl_video_service_watch_travis
