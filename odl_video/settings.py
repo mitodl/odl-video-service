e5a47e286c7d55910965bc164cbcca00eefdbe9e
Doof <mitx-devops@mit.edu>
2025-04-08 16:26:40 +0000

    Release 0.77.0

--- odl_video/settings.py
+++ odl_video/settings.py
@@ -14,7 +14,7 @@ from redbeat import RedBeatScheduler
 from odl_video.envs import get_any, get_bool, get_int, get_key, get_string, parse_env
 from odl_video.sentry import init_sentry
 
-VERSION = "0.76.0"
+VERSION = "0.77.0"
 
 ENVIRONMENT = get_string("ODL_VIDEO_ENVIRONMENT", "dev")
 
