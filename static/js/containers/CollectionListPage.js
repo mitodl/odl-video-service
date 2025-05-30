920b418ec9fed74a4efa53da9f2e680b6322faeb
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2025-05-30 14:52:08 +0500

    feat: search input added for collection page

--- static/js/containers/CollectionListPage.js
+++ static/js/containers/CollectionListPage.js
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom"
 
 import { DIALOGS } from "../constants"
 import * as collectionUiActions from "../actions/collectionUi"
+import { actions } from "../actions"
 import WithDrawer from "./WithDrawer"
 import CollectionFormDialog from "../components/dialogs/CollectionFormDialog"
 import { withDialogs } from "../components/dialogs/hoc"
@@ -34,6 +35,7 @@ export class CollectionListPage extends React.Component<*, void> {
       <div className="collection-list-content">
         <div className="card centered-content">
           <h1 className="mdc-typography--title">My Collections</h1>
+          {this.renderSearchInput()}
           {this.renderCollectionLinks()}
           {this.renderPaginator()}
           {this.renderFormLink()}
@@ -72,6 +74,48 @@ export class CollectionListPage extends React.Component<*, void> {
         Create New Collection
       </a>
     ) : null
+  }
+
+  renderSearchInput() {
+    // Get initial search value from URL if present
+    const params = new URLSearchParams(window.location.search)
+    const searchQuery = params.get('search') || ''
+
+    return (
+      <div className="collection-search">
+        <div className="mdc-text-field mdc-text-field--fullwidth">
+          <input
+            type="text"
+            className="mdc-text-field__input"
+            placeholder="Search collections..."
+            defaultValue={searchQuery}
+            onChange={e => this.handleSearch(e.target.value)}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  handleSearch(searchText) {
+    // Reset to page 1 when searching
+    if (this.props.collectionsPagination.currentPage !== 1) {
+      this.props.collectionsPagination.setCurrentPage(1)
+    }
+
+    // Update URL with search params
+    const params = new URLSearchParams(window.location.search)
+    if (searchText) {
+      params.set('search', searchText)
+    } else {
+      params.delete('search')
+    }
+
+    // Push new URL without reloading the page
+    const newUrl = `${window.location.pathname}?${params.toString()}`
+    window.history.pushState({ path: newUrl }, '', newUrl)
+
+    // Trigger a re-fetch of collections with the search parameter
+    this.props.dispatch(actions.collectionsPagination.getPage({ page: 1 }))
   }
 
   openNewCollectionDialog() {
