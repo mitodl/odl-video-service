088145ab986c513a327d22a111165d4979468248
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2026-05-12 15:50:05 +0500

    fix: keep VideoCard component as functional component

--- static/js/components/VideoCard.js
+++ static/js/components/VideoCard.js
@@ -23,111 +23,102 @@ type VideoCardProps = {
   onReplaceVideo?: Function
 }
 
-class VideoCard extends React.Component<VideoCardProps, void> {
-  dropboxTriggerRef: ?HTMLElement
+const VideoCard = (props: VideoCardProps) => {
+  const { video, isAdmin, isMenuOpen, showShareVideoDialog,
+    showEditVideoDialog, showDeleteVideoDialog,
+    showVideoMenu, hideVideoMenu, onReplaceVideo } = props
 
-  triggerReplaceDropbox() {
-    if (this.dropboxTriggerRef) {
-      this.dropboxTriggerRef.click()
+  let dropboxTriggerEl: ?HTMLElement = null
+  const triggerReplaceDropbox = () => {
+    if (dropboxTriggerEl) {
+      dropboxTriggerEl.click()
     }
   }
 
-  render() {
-    const { video, isAdmin, isMenuOpen, showShareVideoDialog,
-      showEditVideoDialog, showDeleteVideoDialog,
-      showVideoMenu, hideVideoMenu, onReplaceVideo } = this.props
+  let videoDisplay
+  const hasError = videoHasError(video),
+    isProcessing = videoIsProcessing(video)
+  const headerClass = isProcessing || hasError ? "message" : "thumbnail",
+    videoUrl = makeVideoUrl(video.key)
 
-    let videoDisplay
-    const hasError = videoHasError(video),
-      isProcessing = videoIsProcessing(video)
-    const headerClass = isProcessing || hasError ? "message" : "thumbnail",
-      videoUrl = makeVideoUrl(video.key)
-
-    if (isProcessing) {
-      videoDisplay = (
-        <div>
-          <h5>In Progress...</h5>
-          <p>You should receive an email when the video is ready.</p>
-        </div>
-      )
-    } else if (hasError) {
-      videoDisplay = (
-        <div className="error">
-          <h5>Upload failed.</h5>
-          <p>
-            Please{" "}
-            <a href={`mailto:${SETTINGS.support_email_address}`}>contact</a> the
-            ODL Video Services team.
-          </p>
-        </div>
-      )
-    } else {
-      videoDisplay = (
-        <a href={videoUrl}>
-          <img src={makeVideoThumbnailUrl(video)} alt="" />
-        </a>
-      )
-    }
-
-    let menuItems = [
-      { label: "Share", action: showShareVideoDialog.bind(this) }
-    ]
-
-    const inFlight = videoIsInFlight(video)
-
-    if (isAdmin) {
-      menuItems = _.concat(
-        menuItems,
-        { label: "Edit", action: showEditVideoDialog.bind(this) },
-        {
-          label:  "Save To Dropbox",
-          action: saveToDropbox.bind(this, video)
-        },
-        ...(!inFlight && onReplaceVideo ? [{
-          label:  "Replace",
-          action: this.triggerReplaceDropbox.bind(this)
-        }] : []),
-        { label: "Delete", action: showDeleteVideoDialog.bind(this) }
-      )
-    }
-
-    return (
-      <Card className="video-card">
-        <div className={headerClass}>{videoDisplay}</div>
-        <div className="video-card-body">
-          <h2 className="mdc-typography--subheading2">
-            <a href={videoUrl} title={video.title}>
-              {video.title}
-            </a>
-          </h2>
-        </div>
-        <div className="actions">
-          <Menu
-            key={video.key}
-            showMenu={showVideoMenu}
-            closeMenu={hideVideoMenu}
-            open={isMenuOpen}
-            menuItems={menuItems}
-          />
-          {isAdmin && onReplaceVideo && !videoIsInFlight(video) && (
-            <div style={{ display: "none" }}>
-              <DropboxChooser
-                appKey={SETTINGS.dropbox_key}
-                success={files => onReplaceVideo(files[0])}
-                linkType="direct"
-                multiselect={false}
-                extensions={["video"]}
-              >
-                <button ref={el => {
-                  this.dropboxTriggerRef = el
-                }}>replace</button>
-              </DropboxChooser>
-            </div>
-          )}
-        </div>
-      </Card>
+  if (isProcessing) {
+    videoDisplay = (
+      <div>
+        <h5>In Progress...</h5>
+        <p>You should receive an email when the video is ready.</p>
+      </div>
+    )
+  } else if (hasError) {
+    videoDisplay = (
+      <div className="error">
+        <h5>Upload failed.</h5>
+        <p>
+          Please{" "}
+          <a href={`mailto:${SETTINGS.support_email_address}`}>contact</a> the
+          ODL Video Services team.
+        </p>
+      </div>
+    )
+  } else {
+    videoDisplay = (
+      <a href={videoUrl}>
+        <img src={makeVideoThumbnailUrl(video)} alt="" />
+      </a>
     )
   }
+
+  let menuItems = [{ label: "Share", action: showShareVideoDialog }]
+
+  const inFlight = videoIsInFlight(video)
+
+  if (isAdmin) {
+    menuItems = _.concat(
+      menuItems,
+      { label: "Edit", action: showEditVideoDialog },
+      { label: "Save To Dropbox", action: () => saveToDropbox(video) },
+      ...(!inFlight && onReplaceVideo ?
+        [{ label: "Replace", action: triggerReplaceDropbox }] :
+        []),
+      { label: "Delete", action: showDeleteVideoDialog }
+    )
+  }
+
+  return (
+    <Card className="video-card">
+      <div className={headerClass}>{videoDisplay}</div>
+      <div className="video-card-body">
+        <h2 className="mdc-typography--subheading2">
+          <a href={videoUrl} title={video.title}>
+            {video.title}
+          </a>
+        </h2>
+      </div>
+      <div className="actions">
+        <Menu
+          key={video.key}
+          showMenu={showVideoMenu}
+          closeMenu={hideVideoMenu}
+          open={isMenuOpen}
+          menuItems={menuItems}
+        />
+        {isAdmin && onReplaceVideo && !inFlight && (
+          <div style={{ display: "none" }}>
+            <DropboxChooser
+              appKey={SETTINGS.dropbox_key}
+              success={files => onReplaceVideo(files[0])}
+              linkType="direct"
+              multiselect={false}
+              extensions={["video"]}
+            >
+              <button ref={el => {
+                dropboxTriggerEl = el
+              }}>replace</button>
+            </DropboxChooser>
+          </div>
+        )}
+      </div>
+    </Card>
+  )
 }
 
 export default VideoCard
